4d2a622e7f1aeb95f409196e90fcc7804af0ca38
forprojectwork001 <forprojectwork001@gmail.com>
2026-02-18 20:12:16 +0530

    Update FaceAuth.jsx

--- src/components/FaceAuth.jsx
+++ src/components/FaceAuth.jsx
@@ -136,7 +136,7 @@ export default function FaceAuth() {
         Register
       </button>
 
-      <button onClick={login} disabled={!loaded || !cameraReady}>
+      <button onClick={login} style={{backgroundColor:"orange"}} disabled={!loaded || !cameraReady}>
         Login
       </button>
     </div>
